f607d5a383619150795e4448161e975aa4650c75
prabhu <58967128+prabhu9652@users.noreply.github.com>
2022-08-29 12:20:53 +0530

    terraform files modified

--- Terraform-manifests-to-provision-Ansible-clients/c5-01-Public-ec2-Ansible-Clients.tf
+++ Terraform-manifests-to-provision-Ansible-clients/c5-01-Public-ec2-Ansible-Clients.tf
@@ -18,9 +18,20 @@ resource "aws_instance" "Ansible-clients" {
   }
   user_data = <<-EOF
 #! /bin/bash
-sudo su - 
+sudo su -
+# adding ansadmin user
+sudo useradd -m -p ansadmin -s /bin/bash ansadmin
+sudo usermod -a -G sudo ansadmin
+sudo echo ansadmin:ansadmin | chpasswd
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
-sudo cat 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC1cdT8hFsndOBDS5ryIsZzJGsmf5UaCXHnUkjqgZcN7RLM2ZkMR33hDi9jP07DTdKSBUQ98L3YhkRMCxDq2gRH0//IwT1asBst2shnx3tCKLVZxeHX3VQRWt2CrvA8eqJE4/JUJNxlNJMjdXjKy2QmlG/f2uv+RLblkXvMVa2p4KPzpH3NNTKVYSpi/RkWlZBnxPuBHglpN9HBFRNXLj0fVYnvenDRMUYWtZd2v2of4UYKo/DPV5LPwDMc05+dIEbHu63yyxaVv+V66sJ49mzXg70BIZd73gE2EzK4OzF/6E6VJuKMwkKMqVJUxx3gZeLJA2W0sDKoz0gQtruQ1nGj4ItNbOyvBMSRxn0MmzAAZijIbv0DcD20mEcmolnItH0yAaqSkCQtaWDiPQ0H1r6yJaooDgmSiYqf2rxYmMCRo6krW0WuanExBtCx1yDsDcnzETIAGTzAZ+Jo07EdbJlVQyWYSHo2ZSV8lKJu+/w8W1fSLQFW5P7be+Dr4YUQRN8= root@Ansible-terraform-host' >> /home/ubuntu/.ssh/authorized_keys
-sudo chmod 400 /home/ubuntu/.ssh/authorized_keys
+sudo service sshd restart
+
+# adding users to sudoers file
+sudo echo 'ansadmin  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+
+# public key adding under ansadmin user. 
+sudo cat 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC1cdT8hFsndOBDS5ryIsZzJGsmf5UaCXHnUkjqgZcN7RLM2ZkMR33hDi9jP07DTdKSBUQ98L3YhkRMCxDq2gRH0//IwT1asBst2shnx3tCKLVZxeHX3VQRWt2CrvA8eqJE4/JUJNxlNJMjdXjKy2QmlG/f2uv+RLblkXvMVa2p4KPzpH3NNTKVYSpi/RkWlZBnxPuBHglpN9HBFRNXLj0fVYnvenDRMUYWtZd2v2of4UYKo/DPV5LPwDMc05+dIEbHu63yyxaVv+V66sJ49mzXg70BIZd73gE2EzK4OzF/6E6VJuKMwkKMqVJUxx3gZeLJA2W0sDKoz0gQtruQ1nGj4ItNbOyvBMSRxn0MmzAAZijIbv0DcD20mEcmolnItH0yAaqSkCQtaWDiPQ0H1r6yJaooDgmSiYqf2rxYmMCRo6krW0WuanExBtCx1yDsDcnzETIAGTzAZ+Jo07EdbJlVQyWYSHo2ZSV8lKJu+/w8W1fSLQFW5P7be+Dr4YUQRN8= root@Ansible-terraform-host' >> /home/ansadmin/.ssh/authorized_keys
+sudo chown ansadmin:ansadmin /home/ansadmin/.ssh/authorized_keys
+sudo chmod 644 /home/ansadmin/.ssh/authorized_keys
 EOF
 }
