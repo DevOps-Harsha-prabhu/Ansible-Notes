9dac12cc0a8e4a688148ddf1521b2769f91f8b7d
prabhu <58967128+prabhu9652@users.noreply.github.com>
2022-08-30 19:31:42 +0530

    docker swarm manifests added

--- 8.Terraform-manifests-to-provision-Docker-swarm/local_file.tf
+++ 8.Terraform-manifests-to-provision-Docker-swarm/local_file.tf
@@ -1,9 +1,9 @@
 resource "local_file" "hosts_cfg" {
   content = templatefile("hosts.tpl",
     {
-      server1 = aws_instance.Docker-swarm-master-1.public_ip
-      server2 = aws_instance.Docker-swarm-master.0.public_ip
-      server3 = aws_instance.Docker-swarm-master.1.public_ip
+      server1 = aws_instance.Docker-swarm-master-1.0.public_ip
+      server2 = aws_instance.Docker-swarm-master-2-and-3.0.public_ip
+      server3 = aws_instance.Docker-swarm-master-2-and-3.1.public_ip
       server4 = aws_instance.Docker-swarm-workder-nodes.0.public_ip
       server5 = aws_instance.Docker-swarm-workder-nodes.1.public_ip
       server6 = aws_instance.Docker-swarm-workder-nodes.2.public_ip
